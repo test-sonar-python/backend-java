d80d5774b256b033567bc4b2dd946f31e4b60e15
Oscar Chauza <oscaryovany90@gmail.com>
2022-07-28 10:43:46 -0500

    test-sonar

---
+++ src/test/java/com/unicauca/doctorado/cloudgatewayservice/CloudGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicauca.doctorado.cloudgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
